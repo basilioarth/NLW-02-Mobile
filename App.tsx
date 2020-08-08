8a38f10092066288949efc2c4b7d14f19123ed5e
basilioarth <basilio.arth@gmail.com>
2020-08-08 01:53:45 -0300

    Estruturação e Estilização da LandingPage e da StudyTabs

--- App.tsx
+++ App.tsx
@@ -1,21 +1,37 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { AppLoading } from 'expo';
+
+import { Archivo_400Regular, Archivo_700Bold, useFonts } from '@expo-google-fonts/archivo';
+import { Poppins_400Regular, Poppins_600SemiBold } from '@expo-google-fonts/poppins';
+
+import AppStack from './src/routes/AppStack';
+import Landing from './src/pages/Landing';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+  let [fontsLoaded] = useFonts({
+    Archivo_400Regular,
+    Archivo_700Bold,
+    Poppins_400Regular,
+    Poppins_600SemiBold,
+  });
+
+  // As fontes demoram um pouco para carregar. A ideia é que enquanto as fontes ainda estão
+  // carregando, o App n saia da tela de carregamento (quem ativa essa terra de carregamento
+  // é o AppLoading; por isso que precisamos dele). 'fontsLoaded' inicia como false e
+  // só se modifica para true quando todas as fontes são carregadas.
+  if (!fontsLoaded) {
+    return <AppLoading />
+    // Faz com que o App aprensente a tela de carregamento
+  } else {
+    return (
+      <>
+        <AppStack />
+        <StatusBar style="light" />
+      </>
+    );
+  }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+
+
